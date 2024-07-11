39596a65abe96a32735a5f11013c04b50e626669
german.bf <german.bf@avangenio.com>
2024-07-11 14:00:10 -0400

    preparando modelos

--- app/app_Django/abc_Blog/abc_Blog/settings.py
+++ app/app_Django/abc_Blog/abc_Blog/settings.py
@@ -24,7 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'manage_post.apps.ManagePostConfig', 
-    'user.apps.UserConfig',  
+    'user.apps.UserConfig',
+    'django_cleanup.apps.CleanupConfig',
+
 ]
 
 
@@ -113,6 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
